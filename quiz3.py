2d9723023cc1edb869bc0ff755cd2ce9235c6933
gorusair <67059323+gorusair@users.noreply.github.com>
2020-08-07 19:15:45 +0900

    21-30 예제

---
+++ quiz3.py
@@ -0,0 +1,31 @@
+letters ='python'
+print(letters[0],letters[2])
+
+license_plate="24가 2210"
+print(license_plate[3:])
+
+string ="PYTHON"
+print(string[::-1])
+
+string ="홀짝홀짝홀짝"
+print(string[::2])
+
+phone_number ="010-1111-2222"
+phone_number1=phone_number.replace("-"," ")
+print(phone_number1)
+
+phone_number ="010-1111-2222"
+phone_number1=phone_number.replace("-","")
+print(phone_number1)
+
+url = "http://sharebook.kr"
+url_split =url.split(".")
+print(url_split[1])
+
+lang ='python'
+lang[0]='p'
+print(lang)
+
+string='abcd'
+string.replace('b','B')
+print(string)
