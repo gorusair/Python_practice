f86dba56629fcd9421741be7900a2f48cb958cac
gorusair <67059323+gorusair@users.noreply.github.com>
2020-08-07 14:10:01 +0900

    quiz 11-20

--- quiz1.py
+++ quiz1.py
@@ -8,3 +8,32 @@ PER =15.79
 print(시가총액, type(시가총액))
 print(현재가,type(현재가))
 print(PER, type(PER))
+
+s = "hello"
+t = "python"
+print(s+"!", t)
+print(2+2*3)
+
+a ="132"
+print(type(a))
+
+num_str ="720" #형변환
+num_int =int(num_str)
+print(num_int, type(num_int))
+
+num = 100
+num_str=str(num)
+print(num_str, type(num_str))
+
+data ="15.79"
+data =float(data)
+print(data, type(data))
+
+year="2020"
+print(int(year)-3)
+print(int(year)-2)
+print(int(year)-1)
+
+월 =48584
+총금액= 월*36
+print(총금액)
